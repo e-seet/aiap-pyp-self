3dc3bd3c54b7801d8384100a4caa9e24b3cffe00
Ng Chuan Seng <waelectriz@gmail.com>
2024-11-25 21:09:28 +0800

    [Code] Add model duration to model_eval.py

--- HotelNoShowPrediction/task_2/src/model_eval/model_eval.py
+++ HotelNoShowPrediction/task_2/src/model_eval/model_eval.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import time
+
+import setup.duration_cal as duration_cal
 
 from sklearn.metrics import mean_squared_error, r2_score
 from typing import Dict
@@ -12,6 +15,8 @@ def model_evaluation(
     eval_result_dict = {}
 
     for model_name, model in best_estimator_dict.items():
+        model_start_time = time.time()
+        print(f"Processing {model_name} now...")
         ### Predict on test set
         Y_predict = model.predict(X_test)
         ### Calculate evaluation metrics
@@ -19,6 +24,12 @@ def model_evaluation(
         r2 = r2_score(Y_test, Y_predict)
         ### Store results
         eval_result_dict[model_name] = {"Mean Squared Error": mse, "R^2 Score": r2}
+
+        model_end_time = time.time()
+        model_total_time = model_end_time - model_start_time
+        model_duration, model_tag = duration_cal.duration_cal(model_total_time)
+        print(f"{model_name} has run for {model_duration:.3f} {model_tag}!")
+        print()
 
     ## Info -
     ### Mean Squared Error - Measures average squared difference between predicted and actual values
